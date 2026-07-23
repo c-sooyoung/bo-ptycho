6567d8f8adfb108b7c05fde6ac748268e455cf4b
Sooyoung Cheong <64125280+c-sooyoung@users.noreply.github.com>
2026-07-23 15:01:58 +0900

    job update

--- main.py
+++ main.py
@@ -16,21 +16,16 @@ def main(config_yaml):
     os.makedirs(result_dir, exist_ok=True)
     shutil.copy(config_yaml, os.path.join(result_dir, os.path.basename(config_yaml)))
 
-
     randombo = bo.RandomBOEngine(config)
 
     for j in range(10):
         job_config = randombo.ask()
-
         ptycho_engine = ptycho.FoldSlicePtychoEngine(job_config)
         ptycho_engine.run()
         y_value = -np.log(ptycho_engine.metric())
-
         randombo.tell(job_config, y_value)
-
         print('[random] TRAIN_X\n', randombo.state['train_x'])
         print('[random] TRAIN_Y\n', randombo.state['train_y'])
-
 
     sobo = bo.SingleObjectiveBOEngine(config)
     sobo.train_x = randombo.state['train_x']
@@ -38,13 +33,10 @@ def main(config_yaml):
 
     for j in range(config['bo']['max_iterations']):
         job_config = sobo.ask()
-
         ptycho_engine = ptycho.FoldSlicePtychoEngine(job_config)
-        ptycho_engine.run()
+        ptycho_engine.run(header=f"[BO {j:03d}] ")
         y_value = -np.log(ptycho_engine.metric())
-
         sobo.tell(job_config, y_value)
-
         print('[sobo] TRAIN_X\n', sobo.train_x)
         print('[sobo] TRAIN_Y\n', sobo.train_y)
 
@@ -53,7 +45,5 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python bo-ptycho.py <config_yaml>")
         sys.exit(1)
-
     config_yaml = sys.argv[1]
-    
     main(config_yaml)
