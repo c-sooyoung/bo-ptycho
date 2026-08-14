79be9e3ed35a739c4c9579e4e91cb4af70bb7885
Sooyoung Cheong <64125280+c-sooyoung@users.noreply.github.com>
2026-08-14 19:29:34 +0900

    some conveniences

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import os
 import sys
 import yaml
+import shutil
 
 import pipelines
 
 def main(config_yaml):
     with open(config_yaml, 'r') as f:
         config = yaml.safe_load(f)
+
+    result_dir = config["io"]["result_dir"]
+    if os.path.exists(result_dir):
+        shutil.rmtree(result_dir)
+    os.makedirs(result_dir, exist_ok=True)
+    shutil.copy(config_yaml, os.path.join(result_dir, os.path.basename(config_yaml)))
 
     pipelines.job_types[config['job']['type']](config)
     
